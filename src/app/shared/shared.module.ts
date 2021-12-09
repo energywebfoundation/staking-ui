5d298109869ff66def318b38fd38f19cfeadd26d
dawidgil <dawid.gil@j-labs.pl>
2021-12-09 11:56:14 +0100

    del: not needed modules from shared module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AlertModule } from 'ngx-bootstrap/alert';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
-import { RatingModule } from 'ngx-bootstrap/rating';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { PopoverModule } from 'ngx-bootstrap/popover';
-import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { MinifiedDidViewerDirective } from './directives/minified-did-viewer/minified-did-viewer.directive';
 import { MinifiedDidViewerDialogComponent } from './directives/minified-did-viewer/minified-did-viewer-dialog.component';
@@ -23,25 +12,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CopyToClipboardModule } from './directives/copy-to-clipboard/copy-to-clipboard.module';
 import { DidFormatMinifierModule } from './pipes/did-format-minifier/did-format-minifier.module';
@@ -50,29 +30,15 @@ import { DidFormatMinifierModule } from './pipes/did-format-minifier/did-format-
 const MATERIAL_MODULES = [
   MatButtonModule,
   MatButtonToggleModule,
-  MatCardModule,
-  MatChipsModule,
-  MatTableModule,
   MatDialogModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatListModule,
   MatMenuModule,
   MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
   MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSlideToggleModule,
-  MatSliderModule,
-  MatSnackBarModule,
-  MatTabsModule,
-  MatToolbarModule,
   MatTooltipModule,
   MatNativeDateModule,
-  MatStepperModule,
 ];
 
 // https://angular.io/styleguide#!#04-10
@@ -82,18 +48,7 @@ const MATERIAL_MODULES = [
     FormsModule,
     ReactiveFormsModule,
     AlertModule.forRoot(),
-    ButtonsModule.forRoot(),
-    CollapseModule.forRoot(),
-    DatepickerModule.forRoot(),
-    BsDatepickerModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    ModalModule.forRoot(),
-    ProgressbarModule.forRoot(),
-    RatingModule.forRoot(),
-    TabsModule.forRoot(),
-    TooltipModule.forRoot(),
     PopoverModule.forRoot(),
-    TypeaheadModule.forRoot(),
     CopyToClipboardModule,
     DidFormatMinifierModule,
     MATERIAL_MODULES
@@ -108,19 +63,7 @@ const MATERIAL_MODULES = [
     ReactiveFormsModule,
     RouterModule,
     AlertModule,
-    ButtonsModule,
-    CollapseModule,
-    DatepickerModule,
-    BsDatepickerModule,
-    BsDropdownModule,
-    ModalModule,
-    PaginationModule,
-    ProgressbarModule,
-    RatingModule,
-    TabsModule,
-    TooltipModule,
     PopoverModule,
-    TypeaheadModule,
     MinifiedDidViewerDirective,
     MATERIAL_MODULES,
     CopyToClipboardModule,
