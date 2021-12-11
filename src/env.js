93fb5b83d4a02c34d0d21336e9ee90b86d55f276
dawidgil <dawid.gil@j-labs.pl>
2021-12-11 15:11:02 +0100

    Revert "chg: contract and enable checking roles."
    This reverts commit 11380bdb363082cc25655d9e2fade311e81d02e3.

--- src/env.js
+++ src/env.js
@@ -9,9 +9,9 @@
     window.__env.cacheServerUrl = 'https://identitycache-dev.energyweb.org/v1';
     window.__env.natsServerUrl = 'https://identityevents-dev.energyweb.org/';
     window.__env.kmsServerUrl = undefined;
-    window.__env.stakingPoolFactoryAddress = '0x630FA5B60cF5A85321cC9E8cc1e6A32f4dF73e8e';
+    window.__env.stakingPoolFactoryAddress = '0x75006AB5f8f41A73a0E512E0E73dde793cd0378B';
     window.__env.claimManagerAddress = '0xC3dD7ED75779b33F5Cfb709E0aB02b71fbFA3210';
-    window.__env.checkStakingVerification = true;
+    window.__env.checkStakingVerification = false;
     window.__env.showAzureLoginOption = false;
 
 }(this));
