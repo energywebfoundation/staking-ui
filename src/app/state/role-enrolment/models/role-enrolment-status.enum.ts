3f7979cd63b5cff70cbf0c277c5bf33ed8363138
dawidgil <dawid.gil@j-labs.pl>
2021-12-12 22:09:58 +0100

    chg: make enum more descriptive.

--- src/app/state/role-enrolment/models/role-enrolment-status.enum.ts
+++ src/app/state/role-enrolment/models/role-enrolment-status.enum.ts
@@ -1,6 +1,6 @@
 export enum RoleEnrolmentStatus {
-  NOT_ENROLED,
-  ENROLED_NOT_APPROVED,
-  ENROLED_APPROVED,
-  ENROLED_SYNCED
+  NOT_ENROLED = 'Not Enroled',
+  ENROLED_NOT_APPROVED = 'Not Approved',
+  ENROLED_APPROVED = 'Enroled Approved But Not Synced',
+  ENROLED_SYNCED = 'Enroled And Synced'
 }
