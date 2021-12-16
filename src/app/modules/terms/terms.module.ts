1e5fe803690deed0565e9e4db01b55b3e2902e6e
Krzysztof Szostak <christopher.szostak@energyweb.org>
2021-12-16 15:32:17 +0100

    feat: add terms

---
+++ src/app/modules/terms/terms.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {TermsComponent} from "./terms/terms.component";
+
+@NgModule({
+    declarations: [
+        TermsComponent
+    ],
+    imports: [
+        RouterModule.forChild([{path: '', component: TermsComponent}]),
+    ],
+bootstrap: [TermsComponent]
+})
+export class TermsModule {
+}
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { NoPreloading, RouterModule } from '@angular/router';
+import {TermsModule} from "../modules/terms/terms.module";
 
 export const routes = [
   {
     path: '',
     loadChildren: () => import('../modules/ewt-patron/ewt-patron.module').then(m => m.EwtPatronModule)
   },
+  {
+    path: 'terms',
+    loadChildren: () => import('../modules/terms/terms.module').then(m => m.TermsModule)
+  },
   // Not found
-  {path: '**', redirectTo: ''}
+  {path: '**', redirectTo: '/'}
 
 ];
 
