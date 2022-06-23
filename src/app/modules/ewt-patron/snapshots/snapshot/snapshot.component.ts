63e44995a1d061e7b2e7c9e10e8e8f43f0331ac0
dawidgil <dawid.gil@j-labs.pl>
2022-06-23 11:28:20 +0200

    feat: dispatch action on bubble click, adjust bubble colors

--- src/app/modules/ewt-patron/snapshots/snapshot/snapshot.component.ts
+++ src/app/modules/ewt-patron/snapshots/snapshot/snapshot.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
+import { RoleEnrolmentStatus } from '../../../../state/role-enrolment/models/role-enrolment-status.enum';
+import { snapshotInfo } from '../models/snapshot-info';
 
 @Component({
   selector: 'app-snapshot',
   templateUrl: './snapshot.component.html',
-  styleUrls: ['./snapshot.component.scss']
+  styleUrls: ['./snapshot.component.scss'],
 })
 export class SnapshotComponent {
   @Input() number: number;
+  @Input() set status(value: RoleEnrolmentStatus) {
+    this.enrolmentStatus = this.getEnrolmentStatus(value);
+  }
   @Input() type: 'past' | 'present' | 'future';
+
+  @HostBinding('class') enrolmentStatus: string;
+
+  private getEnrolmentStatus(value: RoleEnrolmentStatus): string {
+    return snapshotInfo.has(value) ? snapshotInfo.get(value).cssClass : '';
+  }
+
 }
--- src/app/state/snapshot/snapshot.actions.ts
+++ src/app/state/snapshot/snapshot.actions.ts
@@ -7,14 +7,22 @@ export const checkRevealedSnapshots = createAction(
 
 export const checkRevealedSnapshotsSuccess = createAction(
   '[Snapshot] Check Revealed Snapshots Success',
-  props<{snapshotRoles: Claim[]}>()
+  props<{ snapshotRoles: Claim[] }>()
 );
 
 export const enrolToSnapshots = createAction(
   '[Snapshot] Enrol To Snapshots',
-  props<{claims: string[]}>()
+  props<{ claims: string[] }>()
 );
 
-export const checkSnapshots = createAction(
-  '[Snapshot] Check Snapshots'
-)
+export const checkSnapshots = createAction('[Snapshot] Check Snapshots');
+
+export const enrolToSnapshotRole = createAction(
+  '[Snapshot] Enrol To Snapshot Role',
+  props<{ id: number }>()
+);
+
+export const syncSnapshotEnrolment = createAction(
+  '[Snapshot] Sync Snapshot Role',
+  props<{ id: number }>()
+);
