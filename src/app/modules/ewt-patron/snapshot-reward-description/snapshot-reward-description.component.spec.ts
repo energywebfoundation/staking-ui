0933b36b2e715926eea7168b10f663bc7aad215c
dawidgil <dawid.gil@j-labs.pl>
2022-07-08 11:37:52 +0200

    feat: description for claiming snapshots section

---
+++ src/app/modules/ewt-patron/snapshot-reward-description/snapshot-reward-description.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SnapshotRewardDescriptionComponent } from './snapshot-reward-description.component';
+
+describe('SnapshotRewardDescriptionComponent', () => {
+  let component: SnapshotRewardDescriptionComponent;
+  let fixture: ComponentFixture<SnapshotRewardDescriptionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SnapshotRewardDescriptionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SnapshotRewardDescriptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/ewt-patron/snapshot-reward-description/snapshot-reward-description.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snapshot-reward-description',
+  templateUrl: './snapshot-reward-description.component.html',
+  styleUrls: ['./snapshot-reward-description.component.scss'],
+})
+export class SnapshotRewardDescriptionComponent {}
