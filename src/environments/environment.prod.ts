992d516491573cae056910bf157a62d8f7b0f0ab
dawidgil <dawid.gil@j-labs.pl>
2022-07-27 23:12:11 +0200

    feat: new snapshot role for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -22,7 +22,7 @@ const env: Partial<typeof commonEnv> = {
   currencyName: 'EWT',
   currencySymbol: 'EWT',
   blockExlorerUrl: 'https://explorer.energyweb.org',
-  snapshotRoles: ['snapshot1.roles.consortiapool.apps.energyweb.auth.ewc']
+  snapshotRoles: ['snapshot1.roles.consortiapool.apps.energyweb.auth.ewc', 'snapshot2.roles.consortiapool.apps.energyweb.auth.ewc']
 };
 
 export const environment = { ...commonEnv, ...env };
