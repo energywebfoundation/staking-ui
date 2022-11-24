b8f68c3c977d41463d13861748263e0917cad120
dawidgil <dawid.gil@j-labs.pl>
2022-11-24 11:46:27 +0100

    fix: ew logo

--- src/app/modules/ewt-patron/ew-logo/ew-logo.component.ts
+++ src/app/modules/ewt-patron/ew-logo/ew-logo.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ew-logo',
   templateUrl: './ew-logo.component.html',
   styleUrls: ['./ew-logo.component.scss'],
 })
-export class EwLogoComponent implements OnInit {
-  logoRadialColor1;
-  logoRadialColor2;
-  logoRadialColor3;
-  logoRadialColor4;
-  logoLinearColor1;
-  logoLinearColor2;
+export class EwLogoComponent {
+  @Input() size: 'large' | 'small' = 'large'
+  logoRadialColor1 = this.getColor('--logo-radial-color-1');
+  logoRadialColor2 = this.getColor('--logo-radial-color-2');
+  logoRadialColor3 = this.getColor('--logo-radial-color-3');
+  logoRadialColor4 = this.getColor('--logo-radial-color-4');
 
-  ngOnInit(): void {
-    this.logoRadialColor1 = this.getColor('--logo-radial-color-1');
-    this.logoRadialColor2 = this.getColor('--logo-radial-color-2');
-    this.logoRadialColor3 = this.getColor('--logo-radial-color-3');
-    this.logoRadialColor4 = this.getColor('--logo-radial-color-4');
+  logoLinearColor1 = this.getColor('--logo-linear-color-1');
+  logoLinearColor2 = this.getColor('--logo-linear-color-2');
 
-    this.logoLinearColor1 = this.getColor('--logo-linear-color-1');
-    this.logoLinearColor2 = this.getColor('--logo-linear-color-2');
+  get isBig() {
+    return this.size === 'large';
   }
 
   getColor(property: string) {
